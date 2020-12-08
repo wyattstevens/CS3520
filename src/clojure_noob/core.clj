f92dbdf1ed2416d8071b1ac009aed3b90ea7c601
jcrepes <d00393959@dmail.dixie.edu>
2020-12-08 08:35:49 -0700

    Print message and number of players

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,12 +1,18 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn cl-print [x] (doto x (print)))
+
 (defn hello [name]
   (println "Hello" name))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!")
-  (hello (read-line)))
+  (println "Welcome! Time to play! Try to use all of your letters.")
+  (println "The first player that uses all of their letters wins!")
+  (println "How many players will be playing?")
+  (let [num_players (read-line)]))
+  (for [num_players]
+  (println "Enter the name for player #"))
 
